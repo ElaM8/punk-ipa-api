a561c4f82b9bf4e8b88ad6607ceea5a29d14d411
ElaM8 <daniela.gutperl@gmail.com>
2023-08-23 22:49:21 +0100

    updated getData to fetch all Ipas

--- punk-ipa-api/src/App.tsx
+++ punk-ipa-api/src/App.tsx
@@ -12,17 +12,28 @@ useEffect(() => {
     },[])
 
 const getData = async () => {
-  const res = await fetch("https:api.punkapi.com/v2/beers?per_page=80")
-    const data = await res.json()
-    setIpas(data)
-}
+  let pageNbr: number = 1;
+  let prevResult: Ipa[] = []
+  let allData: Ipa[] = []
+  do {
+      const res = await fetch(`https://api.punkapi.com/v2/beers?page=${pageNbr}&per_page=80`)
+      const data = await res.json();
+      prevResult = data;
+      data.forEach(ipa => {
+        const { id, name, description, image_url, abv, first_brewed, ph } = ipa;
+        allData.push({ id, name, description, image_url, abv, first_brewed, ph })
+      });
+      pageNbr++
+    } while (prevResult.length !== 0)
+      setIpas(allData)
+  }
+  
 
 return (
     <>
     <Navbar ipas={ipas}/>
     <h1>Punk API</h1>
-    <Main ipas={ipas}/>
-    
+    <Main ipas={ipas}/>   
    </>
   )
 }
